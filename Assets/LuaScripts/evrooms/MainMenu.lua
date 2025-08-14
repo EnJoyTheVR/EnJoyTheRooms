c83326d08dea170c5d9e31c2cb9c4acb53b2a000
Maxims <ruslanbelaev36@gmail.com>
2025-08-14 14:21:19 +0300

    теперь при нажатии на играть на начальной сцене, выбирается случайная сцена либо 1 либо 2 либо 3

--- Assets/LuaScripts/evrooms/MainMenu.lua
+++ Assets/LuaScripts/evrooms/MainMenu.lua
@@ -1,3 +1,15 @@
+math.randomseed(os.time())
+
 function LoadGame()
-    EVR:LoadScene("MainGame1")
+
+local scenes = {"MainGame1", "MainGame2", "MainGame3"}
+
+local randomIndex = math.random(1, #scenes)
+
+local targetScene = scenes[randomIndex]
+
+    EVR:LoadScene(targetScene)
+    
+    print("zagruzaev scenu: " .. targetScene)
+
 end
